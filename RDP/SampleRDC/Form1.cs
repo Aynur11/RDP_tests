601e5f9fbe33be3b0258b0a771795272cbc578dc
Aynur <ainur345@yandex.ru>
2018-04-22 23:57:07 +0500

    First commit

---
+++ RDP/SampleRDC/Form1.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using MSTSCLib;
+
+namespace SampleRDC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                rdp.Server = txtServer.Text;
+                rdp.UserName = txtUserName.Text;
+                rdp.AdvancedSettings8.EnableCredSspSupport = true;
+                IMsTscNonScriptable secured = (IMsTscNonScriptable)rdp.GetOcx();
+                secured.ClearTextPassword = txtPassword.Text;
+                rdp.Connect();
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show("Error Connecting", "Error connecting to remote desktop " + txtServer.Text + " Error:  " + Ex.Message,MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void Connect(string IP)
+        {
+            try
+            {
+                rdp.Server = IP;
+                rdp.UserName = "Test";
+                rdp.AdvancedSettings8.EnableCredSspSupport = true;
+                IMsTscNonScriptable secured = (IMsTscNonScriptable)rdp.GetOcx();
+                secured.ClearTextPassword = "111111";
+                rdp.Connect();
+            }
+            catch (Exception Ex)
+            {
+                //Console.WriteLine("Error Connecting", "Error connecting to remote desktop " + txtServer.Text + " Error:  " + Ex.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                MessageBox.Show("Error Connecting", "Error connecting to remote desktop " + txtServer.Text + " Error:  " + Ex.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Check if connected before disconnecting
+                if (rdp.Connected.ToString() == "1")
+                    rdp.Disconnect();
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show("Error Disconnecting", "Error disconnecting from remote desktop " + txtServer.Text + " Error:  " + Ex.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
